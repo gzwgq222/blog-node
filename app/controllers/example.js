21ed7fd26020f0be886ec51989478f06bc796fce
Gong <445722156@qq.com>
2019-03-04 23:24:57 +0800

    新增、查询数据

--- app/controllers/example.js
+++ app/controllers/example.js
@@ -2,56 +2,56 @@
 const Example = require('./../models/example');
 
 const getExample = async function(next) {
-  const list = await Example.find({ _id: '5c7b960676c5a144b5627488' });
+  const data = await Example.find();
   console.log(1111)
   this.status = 200;
   this.body = {
     code: 1000,
-    test: list,
-    data: [
-      {
-        key: '1',
-        name: '测试1',
-        age: 18,
-        address: 'New York No. 1 Lake Park',
-        tags: ['nice', 'developer'],
-      },
-      {
-        key: '2',
-        name: '灰太狼',
-        age: 32,
-        address: 'London No. 1 Lake Park',
-        tags: ['node', 'koa', 'mongodb'],
-      },
-      {
-        key: '3',
-        name: '真香',
-        age: 321,
-        address: 'Sidney No. 1 Lake Park',
-        tags: ['vue', 'react'],
-      }
-    ]
+    data
+    // data: [
+    //   {
+    //     key: '1',
+    //     name: '这都是什么生活',
+    //     age: 18,
+    //     address: 'New York No. 1 Lake Park',
+    //     tags: ['nice', 'developer'],
+    //   },
+    //   {
+    //     key: '2',
+    //     name: '灰太狼',
+    //     age: 32,
+    //     address: 'London No. 1 Lake Park',
+    //     tags: ['node', 'koa', 'mongodb'],
+    //   },
+    //   {
+    //     key: '3',
+    //     name: '真香',
+    //     age: 321,
+    //     address: 'Sidney No. 1 Lake Park',
+    //     tags: ['vue', 'react'],
+    //   }
+    // ]
   }
 }
 
-const postExample = async (ctx, next) => {
-  const req = ctx.request.body;
+const postExample = async function(next) {
+  this.status = 200;
+  const req = this.request.body
+  console.log(req)
+  // if (!req.msg || typeof req.msg != 'string') {
+  //   this.status = 401;
+  //   this.body = {
+  //     msg: 'post request!!',
+  //     desc: `parameter error！！msg: ${req.msg}`,
+  //     data: req
+  //   }
+  //   return;
+  // }
 
-  ctx.status = 200;
-  if (!req.msg || typeof req.msg != 'string') {
-    ctx.status = 401;
-    ctx.body = {
-      msg: 'post request!!',
-      desc: `parameter error！！msg: ${req.msg}`,
-      data: req
-    }
-    return;
-  }
+  const result = await Example.create(req);
 
-  const result = await Example_col.create({msg: req.msg});
-
-  ctx.body = {
-    msg: 'post request!!',
+  this.body = {
+    code: 1000,
     desc: 'insert success!',
     data: result
   }
--- app/models/example.js
+++ app/models/example.js
@@ -3,10 +3,14 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 const exampleSchema = new Schema({
-  msg: {
+  name: {
     type: String,
     required: true
   },
+  title: {
+    type: String,
+    required: true
+  }
 }, { collection: 'example', versionKey: false});
 
 module.exports = mongoose.model('example', exampleSchema);
