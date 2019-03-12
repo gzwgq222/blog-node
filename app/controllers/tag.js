ecfd8d5a9a06ae35b9fb4b5c75a2cc9be3256791
Gong <445722156@qq.com>
2019-03-12 23:56:01 +0800

    查询条数 页数 排序 total

--- app/controllers/tag.js
+++ app/controllers/tag.js
@@ -1,14 +1,28 @@
 const Tag = require('../models/tag')
 
 const taglist = async function() {
-  let name = this.request.query.name
-  const page = Number(this.request.query.page) || 10
-  const params = name ? {name} : {}
-  const data = await Tag.find(params, {_id: 0}).limit(page)
+  const name = this.request.query.name,
+        page = Number(this.request.query.page) || 10,
+        /**
+         * 正则表达式中使用变量
+         * 一定要使用eval将组合的字符串进行转换
+         * 不能直接将字符串拼接后传入给表达式
+         * 否则没有报错信息，只是结果为空
+         * 以下两种写法等同
+         */
+        params = name ? {name: eval(`/${name}/i`)} : {},
+        // params = name ? {name: {$regex: name, $options: '$ix'}} : {},
+        total = await Tag.find()
+                         .count(),
+        data = await Tag.find(params, {_id: 0})
+                        .limit(page)
+                        .skip()
+                        .sort({id: 1})
   this.status = 200
   this.body = {
     code: 1000,
-    data
+    data,
+    total
   }
 
 }
